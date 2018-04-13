a2e57b26f47f7d25ccdba47859873ead9d74ab77
bamboovnth <mrtuan1996toithatngoc@gmail.com>
2018-04-13 15:26:27 +0700

    test

--- WebbChat/urls.py
+++ WebbChat/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [
+urlpatterns =[
     path('', views.index, name='index'),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login')
